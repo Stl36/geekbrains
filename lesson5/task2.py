4be73786a2037ccc79499d360fd257c71ef9b253
AlekseyShmykov <shmykov.job@gmail.com>
2021-02-01 21:08:15 +0300

    task 2 some update task 3 comleted

--- lesson5/task2.py
+++ lesson5/task2.py
@@ -8,8 +8,8 @@ source_f = open("new_f2.txt", "r")
 my_f = []
 for line in source_f:
     my_f.append(line.replace("\n", ""))
+source_f.close()
 number_of_string = len(my_f)
-
 talmud = []
 for el in my_f:
     k = len(el.split())
