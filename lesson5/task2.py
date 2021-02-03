7c596824a73eb2eae9db0ae28d25fef9f3bc7d96
AlekseyShmykov <shmykov.job@gmail.com>
2021-02-03 19:54:49 +0300

    task 4 completed + same 3, 4 update

--- lesson5/task2.py
+++ lesson5/task2.py
@@ -8,12 +8,14 @@ source_f = open("new_f2.txt", "r")
 my_f = []
 for line in source_f:
     my_f.append(line.replace("\n", ""))
+
 source_f.close()
 number_of_string = len(my_f)
 talmud = []
 for el in my_f:
     k = len(el.split())
     talmud.append(k)
+
 print(f"Строки исходного файла {my_f}\n"
       f"Количество строк {number_of_string}\n"
       f"Количество слов в каждой строке соответственно {talmud}")
--- lesson5/task4.py
+++ lesson5/task4.py
@@ -1,3 +1,4 @@
+# coding: utf8
 """
 4. Создать (не программно) текстовый файл со следующим содержимым:
 One — 1
@@ -8,3 +9,19 @@ Four — 4
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл.
 """
+
+
+my_l = []
+with open(r"new_f4.txt", "r") as f_obj:
+    for line in f_obj:
+        my_l.append(line.split())
+rus_l = ["Один", "Два", "Три", "Четыре"]
+
+for id, el in enumerate(my_l):
+    el[0] = rus_l[id]
+
+with open("new_f4_out.txt", "a") as f_out:
+    for line in my_l:
+        print(f"{line[0]} {line[1]} {line[2]}", file=f_out)
+
+print(my_l)
