04d537fbf7ccf452ba231d84b911a4d55f5762fd
AlekseyShmykov <shmykov.job@gmail.com>
2021-02-03 23:31:52 +0300

    task 6 completed

--- lesson5/task6.py
+++ lesson5/task6.py
@@ -13,3 +13,42 @@
 Пример словаря:
 
 {“Информатика”: 170, “Физика”: 40, “Физкультура”: 30}"""
+
+
+# coding: utf8
+
+
+def get_course_name(line_: str, char_: str):
+    return line_[: line_.find(char_)]
+
+
+def get_number_from_line(line_: str):
+    i = 0
+    c = ''
+    numbers_set = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+    while i < len(line_):
+        if line_[i] in numbers_set:
+            c = c + line_[i]
+        i += 1
+    if c == '':
+        return 0
+    else:
+        return int(c)
+
+
+def split_line(line_: str):
+    return sum(get_number_from_line(el) for el in line_.split())
+
+
+my_l = []
+with open("new_f6.txt", "r", encoding="utf8") as f_obj:
+    for line in f_obj:
+        my_l.append(line)
+print(my_l)
+final_dict = {}
+char = ":"
+for line in my_l:
+    final_dict[get_course_name(line, char)] = split_line(line)
+
+for el in final_dict:
+    print(el, final_dict[el])
