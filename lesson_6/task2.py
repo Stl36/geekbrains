0719b758a7eec5b5863c746df44229dab9b70a02
AlekseyShmykov <shmykov.job@gmail.com>
2021-02-07 15:26:00 +0300

    task 2 completed

--- lesson_6/task2.py
+++ lesson_6/task2.py
@@ -12,3 +12,16 @@
 Например: 20 м*5000 м*25 кг*5 см = 12500 т.
 
 """
+class Road:
+    def __init__(self, l, w, d = 25, t = 5):
+        self.__length = l
+        self.__width = w
+        self.__density = d
+        self.__thickness = t
+    def calc_mass(self, t = 5):
+        self.__thickness = t
+        return (self.__length * self.__width * self.__density * self.__thickness)
+
+el_1 = Road(5000, 20)
+print(el_1.__dict__)
+print(el_1.calc_mass(25))
