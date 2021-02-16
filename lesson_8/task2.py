4ccf09c953aef1cfe3b43c4f13d1ef24fd83d957
AlekseyShmykov <shmykov.job@gmail.com>
2021-02-16 15:35:16 +0300

    task2 done

--- lesson_8/task2.py
+++ lesson_8/task2.py
@@ -3,3 +3,21 @@
 Проверьте его работу на данных, вводимых пользователем.
 При вводе нуля в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+
+
+class MyZeroDivisionError(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+my_number = 89087
+
+try:
+    my_divisor = input("Введите делитель ")
+    my_divisor = int(my_divisor)
+    if my_divisor == 0:
+        print(MyZeroDivisionError("Зафиксирована попытка деления на ноль. Записал IP, за Вами выехали"))
+    else:
+        print(my_number / my_divisor)
+
+except Exception as myerr:
+    print(myerr)
